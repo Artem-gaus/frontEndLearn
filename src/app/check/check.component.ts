2faffdeb361c7c738949cbe05988c996c6ab1015
violinist-216 <mtomko216@gmail.com>
2018-06-18 17:48:48 +0300

    clearn repositories

--- src/app/check/check.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-check',
-  templateUrl: './check.component.html',
-  styleUrls: ['./check.component.css']
-})
-export class CheckComponent{
-}
